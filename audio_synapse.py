d157cada512b6184162253bfb565be89db052c86
biggiebk <biggiebk@mymadlab.net>
2023-11-26 10:45:02 -0800

    Pass a list of files for a play list

--- audio_synapse.py
+++ audio_synapse.py
@@ -4,6 +4,7 @@ Description: Contains audio modules
 import json
 import vlc
 import time
+import ctypes
 from beartype import beartype
 from rupert.shared.synapse import Synapse
 
@@ -50,7 +51,8 @@ class RupertAudioPlayer():
 		self.media_list_player = None
 		self.media = None
 		self.control_dict = None
-
+		self.eos = None
+	
 	@beartype
 	def bye(self) -> None:
 		"""Cleans up the the current player"""
@@ -71,8 +73,8 @@ class RupertAudioPlayer():
 			control_dict = Dictionary detailing what controls to execute
 		"""
 		self.control_dict = control_dict
-
-		if 'play_track' in self.control_dict:
+		print(f"  - control f{control_dict}")
+		if 'play_tracks' in self.control_dict:
 			self.__set_media()
 
 		if 'play' in self.control_dict:
@@ -86,6 +88,102 @@ class RupertAudioPlayer():
 			self.__set_volume()
 
 ## Private methods
+
+	@beartype
+	def __event_played(self, event):
+		print("__event_played!")
+		print(f"  {type(event.u)}")
+
+	def __event_stopped(self, event):
+		print("__event_stopped!")
+
+	def __event_ItemAdded(self, event):
+		print("__ItemAdded!")
+
+	def __event_WillAddItem(self, event):
+		print("__WillAddItem!")
+
+	def __event_ItemDeleted(self, event):
+		print("__ItemDeleted!")
+
+	def __event_WillDeleteItem(self, event):
+		print("__WillDeleteItem!")
+
+	def __event_EndReached(self, event):
+		print("__EndReached!")
+
+	def __event_ViewItemAdded(self, event):
+		print("__ViewItemAdded!")
+
+	def __event_ViewWillAddItem(self, event):
+		print("__ViewWillAddItem!")
+
+	def __event_ViewItemDeleted(self, event):
+		print("__ViewItemDeleted!")
+
+	def __event_ViewWillDeleteItem(self, event):
+		print("__ViewWillDeleteItem!")
+
+	def __event_NextItemSet(self, event):
+		print("__NextItemSet!")
+		print(f"  {event.u.new_length}")
+
+	def __event_VlmMediaAdded(self, event):
+		print("__event_VlmMediaAdded!")
+
+	def __event_VlmMediaRemoved(self, event):
+		print("__event_VlmMediaRemoved!")
+
+	def __event_VlmMediaChanged(self, event):
+		print("__event_VlmMediaChanged!")
+
+	def __event_VlmMediaInstanceStarted(self, event):
+		print("__event_VlmMediaInstanceStarted!")
+
+	def __event_VlmMediaInstanceStopped(self, event):
+		print("__event_VlmMediaInstanceStopped!")
+
+	def __event_VlmMediaInstanceStatusInit(self, event):
+		print("__event_VlmMediaInstanceStatusInit!")
+
+	def __event_VlmMediaInstanceStatusOpening(self, event):
+		print("__event_VlmMediaInstanceStatusOpening!")
+
+	def __event_VlmMediaInstanceStatusPlaying(self, event):
+		print("__event_VlmMediaInstanceStatusPlaying!")
+
+	def __event_VlmMediaInstanceStatusPause(self, event):
+		print("__event_VlmMediaInstanceStatusPause!")
+
+	def __event_VlmMediaInstanceStatusEnd(self, event):
+		print("__event_VlmMediaInstanceStatusEnd!")
+
+	def __event_VlmMediaInstanceStatusError(self, event):
+		print("__event_VlmMediaInstanceStatusError!")
+
+	def __event_MediaMetaChanged(self, event):
+		print("__event_MediaMetaChanged!")
+
+	def __event_MediaSubItemAdded(self, event):
+		print("__event_MediaSubItemAdded!")
+
+	def __event_MediaDurationChanged(self, event):
+		print("__event_MediaDurationChanged!")
+
+	def __event_MediaParsedChanged(self, event):
+		print("__event_MediaParsedChanged!")
+
+	def __event_MediaFreed(self, event):
+		print("__event_MediaFreed!")
+
+	def __event_MediaStateChanged(self, event):
+		print("__event_MediaStateChanged!")
+
+	def __event_MediaSubItemTreeAdded(self, event):
+		print("__event_MediaSubItemTreeAdded!")
+
+
+
 	@beartype
 	def __play_stop_pause(self) -> None:
 		"""
@@ -126,16 +224,50 @@ class RupertAudioPlayer():
 			Sets the playing media
 		"""
 		# creating a new media list
+		print(f"Playing {self.control_dict['play_tracks']}")
 		self.media_list = self.player.media_list_new()
 		# creating a media player object
 		self.media_list_player = self.player.media_list_player_new()
 		# creating a new media
-		self.media = self.player.media_new(self.control_dict['play_track'])
-		# adding media to media list
-		self.media_list.add_media(self.media)
+		# if array
+		if isinstance(self.control_dict['play_tracks'], list):
+			for media in self.control_dict['play_tracks']:
+				print(media)
+				self.media = self.player.media_new(media)
+				self.media_list.add_media(self.media)
+		else:
+			print("Not a list")
+			self.media = self.player.media_new(self.control_dict['play_track'])
+			# adding media to media list
+			self.media_list.add_media(self.media)
+
 		# setting media list to the mediaplayer
 		self.media_list_player.set_media_list(self.media_list)
+		media_events = self.media.event_manager()
+	
+		# Event callbacks.
+		events = self.media_list_player.event_manager()
+		media_events = self.media.event_manager()
+		events.event_attach(vlc.EventType.MediaListPlayerPlayed, self.__event_played)
+		events.event_attach(vlc.EventType.MediaListPlayerStopped, self.__event_stopped)
+		events.event_attach(vlc.EventType.MediaListItemAdded, self.__event_ItemAdded)
+		events.event_attach(vlc.EventType.MediaListWillAddItem, self.__event_WillAddItem)
+		events.event_attach(vlc.EventType.MediaListItemDeleted, self.__event_ItemDeleted)
+		events.event_attach(vlc.EventType.MediaListWillDeleteItem, self.__event_WillDeleteItem)
+		events.event_attach(vlc.EventType.MediaListEndReached, self.__event_EndReached)
+		events.event_attach(vlc.EventType.MediaListViewItemAdded, self.__event_ViewItemAdded)
+		events.event_attach(vlc.EventType.MediaListViewWillAddItem, self.__event_ViewWillAddItem)
+		events.event_attach(vlc.EventType.MediaListViewItemDeleted, self.__event_ViewItemDeleted)
+		events.event_attach(vlc.EventType.MediaListViewWillDeleteItem, self.__event_ViewWillDeleteItem)
+		events.event_attach(vlc.EventType.MediaListPlayerNextItemSet, self.__event_NextItemSet)
 
+		media_events.event_attach(vlc.EventType.MediaMetaChanged, self.__event_MediaMetaChanged)
+		media_events.event_attach(vlc.EventType.MediaSubItemAdded, self.__event_MediaSubItemAdded)
+		media_events.event_attach(vlc.EventType.MediaDurationChanged, self.__event_MediaDurationChanged)
+		media_events.event_attach(vlc.EventType.MediaParsedChanged, self.__event_MediaParsedChanged)
+		media_events.event_attach(vlc.EventType.MediaFreed, self.__event_MediaFreed)
+		media_events.event_attach(vlc.EventType.MediaStateChanged, self.__event_MediaStateChanged)
+		media_events.event_attach(vlc.EventType.MediaSubItemTreeAdded, self.__event_MediaSubItemTreeAdded)
 
 	@beartype
 	def __set_volume(self) -> None:
--- www/flask/rupert.py
+++ www/flask/rupert.py
@@ -9,12 +9,26 @@ app = Flask(__name__)
 @app.route("/audio/play/<room>/<file>/<volume>")
 def play(room, file, volume):
   stop =  { "event_type": "control", "play": "stop" }
-  play_track = { "event_type": "control", "play_track": f"/home/rupert/Music/Relax/{escape(file)}.mp3", "return_topic": "debug_topic", "volume": escape(volume), "play": "play" }
+  play_track = { "event_type": "control", "play_tracks": [f"/home/rupert/Music/Relax/{escape(file)}.mp3"], "return_topic": "debug_topic", "volume": escape(volume), "play": "play" }
   kafka_producer = Synapse('/web/cfg/settings.json')
   kafka_producer.send("bedroom_audio_alpha",json.dumps(stop))
   time.sleep(1)
   kafka_producer.send("bedroom_audio_alpha",json.dumps(play_track)) 
   return f"Play Room: {escape(room)} File: {escape(file)}"
+
+
+@app.route("/audio/play/<room>/<file>/<volume>")
+def play_list(room, file, volume):
+  with open(f"lists/{escape(file)}", 'r', encoding='utf-8') as list_file:
+    list_file_json = list_file.read()
+  list = json.loads(list_file_json)
+  stop =  { "event_type": "control", "play": "stop" }
+  play_track = { "event_type": "control", "play_tracks": list, "return_topic": "debug_topic", "volume": escape(volume), "play": "play" }
+  kafka_producer = Synapse('/web/cfg/settings.json')
+  kafka_producer.send("bedroom_audio_alpha",json.dumps(stop))
+  time.sleep(1)
+  kafka_producer.send("bedroom_audio_alpha",json.dumps(play_track)) 
+  return f"Play Room: {escape(room)} List: {escape(file)}"
 
 @app.route("/audio/stop/<room>")
 def stop(room):
